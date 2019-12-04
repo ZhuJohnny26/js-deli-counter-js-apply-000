999a0356489188677f8aeae3551f186a006b3b67
johnny zhu <johnny.zhu26@gmail.com>
2019-12-04 17:27:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ function nowServing(line){
    
   }
 }
+
+function currentLine(line){
+  if(line.length === 0){
+    return "The line is currently empty"
+  }
+  var i 
+  var string = "The line is currently: 1. " + line[0]
+  for(i = 1; i < line.length; i++){
+    string = string + ", " + i+1 + ". " + line[i]
+  }
+  return string
+}
